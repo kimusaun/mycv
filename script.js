a883a0c75f68a8706b178b2676c70419752b3ebf
unknown <kim.rimsamut@gmail.com>
2023-10-21 18:51:50 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function NewTab_FB() {
+    window.open("https://www.facebook.com/profile.php?id=61552225987273", "_blank");
+}
+function NewTab_mail() {
+    
+    navigator.clipboard.writeText('kim.rimsamut@gmail.com');
+    alert("Copy Email to clipboard");
+}
+
